141c1ed4498cae8c5eb96ce253656f8498686326
Jean-Étienne <git@jeanetienne.net>
2016-11-06 18:48:59 +1100

    Initial Xcode project

---
+++ Bee/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Bee
+//
+//  Created by Jean-Étienne on 6/11/16.
+//  Copyright © 2016 Jean-Étienne. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
