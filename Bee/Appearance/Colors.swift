a18259225029d9c2df807a7e435a7acceac8fedf
Jean-Étienne <git@jeanetienne.net>
2017-05-24 16:53:09 +1000

    Better color theme management and UI tooling
    - Collection of Primary/Secondary/Tertiary/etc. colours
    - UITextField subclass to adjust padding
    - Added @IBInspectable for border color/width & corner radius

---
+++ Bee/Appearance/Colors.swift
@@ -0,0 +1,50 @@
+//
+//  Colors.swift
+//  Bee
+//
+//  Created by Jean-Étienne on 26/5/17.
+//  Copyright © 2017 Jean-Étienne. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    
+    static var primary: UIColor {
+        return UIColor.pastelYellow
+    }
+    
+    static var secondary: UIColor {
+        return UIColor.pastelRed
+    }
+    
+    static var tertiary: UIColor {
+        return UIColor.almostWhite
+    }
+    
+    static var faded: UIColor {
+        return UIColor.fadedDarkYellow
+    }
+    
+}
+
+
+extension UIColor {
+    
+    static var pastelYellow: UIColor {
+        return UIColor(red: 255/255, green: 220/255, blue: 115/255, alpha: 1)
+    }
+    
+    static var pastelRed: UIColor {
+        return UIColor(red: 255/255, green: 143/255, blue: 115/255, alpha: 1)
+    }
+    
+    static var almostWhite: UIColor {
+        return UIColor(red: 255/255, green: 254/255, blue: 252/255, alpha: 1)
+    }
+    
+    static var fadedDarkYellow: UIColor {
+        return UIColor(red: 85/255, green: 82/255, blue: 72/255, alpha: 1)
+    }
+    
+}
---
+++ Bee/Appearance/UIView+Inspectables.swift
@@ -0,0 +1,92 @@
+//
+//  UIView+Inspectables.swift
+//  Bee
+//
+//  Created by Jean-Étienne on 24/5/17.
+//  Copyright © 2017 Jean-Étienne. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+extension UIView {
+    
+    @IBInspectable public var backgroundColorName: String {
+        
+        get {
+            return name(forColor: backgroundColor!)
+        }
+        
+        set(colorName) {
+            backgroundColor = color(withName: colorName)
+        }
+        
+    }
+    
+    @IBInspectable public var borderColorName: String {
+        
+        get {
+            return name(forColor: UIColor(cgColor: layer.borderColor!))
+        }
+        
+        set(colorName) {
+            layer.borderColor = color(withName: colorName).cgColor
+        }
+        
+    }
+    
+    @IBInspectable public var borderWidth: CGFloat {
+        
+        get {
+            return self.layer.borderWidth
+        }
+        
+        set(newWidth) {
+            layer.borderWidth = newWidth
+        }
+        
+    }
+    
+    @IBInspectable public var cornerRadius: CGFloat {
+        
+        get {
+            return self.layer.cornerRadius
+        }
+        
+        set(newRadius) {
+            layer.cornerRadius = newRadius
+            layer.masksToBounds = newRadius > 0
+        }
+        
+    }
+    
+    private var colors: [String: UIColor] {
+        return [
+            "Primary": UIColor.primary,
+            "Secondary": UIColor.secondary,
+            "Tertiary": UIColor.tertiary,
+            "Faded": UIColor.faded
+        ]
+    }
+    
+    // MARK: - Private helpers
+    private func color(withName aName: String) -> UIColor {
+        for (colorName, color) in colors {
+            if aName.lowercased() == colorName.lowercased() {
+                return color
+            }
+        }
+        
+        return UIColor.clear
+    }
+    
+    private func name(forColor aColor: UIColor) -> String {
+        for (colorName, color) in colors {
+            if aColor == color {
+                return colorName
+            }
+        }
+        
+        return "Unknown"
+    }
+}
